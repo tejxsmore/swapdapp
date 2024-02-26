313a0a342bd6879a7ba10f763fae08223353acb2
tejxsmore <tejasmore1104@gmail.com>
2024-02-26 11:15:09 +0530

    Swap executed

--- app/components/Swap.tsx
+++ app/components/Swap.tsx
@@ -1,26 +1,49 @@
 "use client";
 import useSWR from "swr";
-import Image from "next/image";
+import axios from "axios";
 import { Key, useState } from "react";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
-const fetcher = (url: string | URL | Request) =>
-  fetch(url).then((res) => res.json());
+const fetcherWithHeaders = (url: string) => {
+  return axios
+    .get(`${url}`, {
+      headers: {
+        api_key: process.env.API_KEY,
+        secret_key: process.env.SECRET_KEY,
+      },
+    })
+    .then((response) => response.data);
+};
 
 export default function Swap() {
   const [token, setToken] = useState(false);
   const [tokenSwap, setTokenSwap] = useState(false);
   const [selectToken, setSelectToken] = useState("");
+  const [tokenPrice, setTokenPrice] = useState(0);
+  const [tokenSwapPrice, setTokenSwapPrice] = useState(0);
   const [selectTokenSwap, setSelectTokenSwap] = useState("");
   const { data } = useSWR(
-    `https://tokens.coingecko.com/uniswap/all.json`,
-    fetcher
+    `https://api.wazirx.com/sapi/v1/tickers/24hr`,
+    (url) => fetcherWithHeaders(url)
   );
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const number = document.getElementById("number") as HTMLInputElement;
+    const state = document.getElementById("state") as HTMLInputElement;
+    const wholeTokenPrice = tokenPrice * Number(number?.value);
+    const wholeTokenSwapPrice = Number(
+      wholeTokenPrice / tokenSwapPrice
+    ).toFixed(2);
+
+    state.value = "" + wholeTokenSwapPrice;
+  };
 
   return (
     <div className="bg-dark flex justify-center items-center min-h-screen p-4">
-      <div
+      <form
         id="swap"
+        onSubmit={handleSubmit}
         className="bg-teal text-dark p-8 rounded-2xl md:max-w-md w-full static"
       >
         <div className="flex items-center p-2 pl-4 bg-white mb-4 rounded-xl">
@@ -33,10 +56,17 @@ export default function Swap() {
               selectToken != "" ? "font-medium" : "font-normal"
             }`}
           >
-            {selectToken === "" ? "Select a token" : selectToken}
+            {selectToken === "" ? "Select a token" : selectToken.toUpperCase()}
           </h3>
           <input
             type="number"
+            id="number"
+            onChange={() => {
+              const state = document.getElementById(
+                "state"
+              ) as HTMLInputElement;
+              state.value = "";
+            }}
             placeholder="amount"
             className="w-1/2 px-4 py-2 bg-light rounded-lg focus:outline-none"
           />
@@ -52,23 +82,29 @@ export default function Swap() {
                 <IoIosCloseCircleOutline />
               </p>
             </div>
-            {data?.tokens.map(
+            {data?.map(
               (token: {
-                address: Key | null | undefined;
                 symbol: string;
-                name: string | undefined;
+                baseAsset: string;
+                openPrice: number;
               }) => (
                 <div
-                  key={token.address}
+                  key={token.symbol}
                   className="flex px-4 py-2 border-b border-grey"
                   onClick={() => {
-                    setSelectToken(token?.symbol);
                     setToken(false);
+                    setSelectToken(token?.baseAsset);
+                    setTokenPrice(token?.openPrice);
+                    const state = document.getElementById(
+                      "state"
+                    ) as HTMLInputElement;
+                    state.value = "";
                   }}
                 >
                   <p>
-                    <span className="font-medium">{token.symbol}</span> -{" "}
-                    {token.name}
+                    <span className="font-medium">
+                      {token.baseAsset.toUpperCase()}
+                    </span>
                   </p>
                 </div>
               )
@@ -79,6 +115,9 @@ export default function Swap() {
           onClick={() => {
             setTokenSwap(!tokenSwap);
             setToken(false);
+            setTokenSwapPrice(0);
+            const state = document.getElementById("state") as HTMLInputElement;
+            state.value = "";
           }}
           className="flex items-center p-2 pl-4 bg-white mb-4 rounded-xl"
         >
@@ -87,10 +126,13 @@ export default function Swap() {
               selectTokenSwap != "" ? "font-medium" : "font-normal"
             }`}
           >
-            {selectTokenSwap === "" ? "Select a token" : selectTokenSwap}
+            {selectTokenSwap === ""
+              ? "Select a token"
+              : selectTokenSwap.toUpperCase()}
           </h3>
           <input
             type="number"
+            id="state"
             placeholder="amount"
             className="w-1/2 px-4 py-2 bg-light rounded-lg focus:outline-none"
           />
@@ -106,23 +148,25 @@ export default function Swap() {
                 <IoIosCloseCircleOutline />
               </p>
             </div>
-            {data?.tokens.map(
+            {data?.map(
               (token: {
-                address: Key | null | undefined;
                 symbol: string;
-                name: string | undefined;
+                baseAsset: string;
+                openPrice: number;
               }) => (
                 <div
-                  key={token.address}
+                  key={token.symbol}
                   className="flex px-4 py-2 border-b border-grey"
                   onClick={() => {
-                    setSelectTokenSwap(token?.symbol);
                     setTokenSwap(false);
+                    setSelectTokenSwap(token?.baseAsset);
+                    setTokenSwapPrice(token?.openPrice);
                   }}
                 >
                   <p>
-                    <span className="font-medium">{token.symbol}</span> -{" "}
-                    {token.name}
+                    <span className="font-medium">
+                      {token.baseAsset.toUpperCase()}
+                    </span>
                   </p>
                 </div>
               )
@@ -134,7 +178,15 @@ export default function Swap() {
         <button className="p-4 w-full bg-dark text-teal hover:opacity-95 rounded-xl text-lg font-semibold italic">
           SWAP
         </button>
-      </div>
+      </form>
     </div>
   );
 }
+
+// const res = await fetch("https://api.wazirx.com/sapi/v1/tickers/24hr", {
+//       cache: "no-store",
+//       headers: {
+//         api_key: process.env.API_KEY,
+//         secret_key: process.env.SECRET_KEY,
+//       },
+//     });
